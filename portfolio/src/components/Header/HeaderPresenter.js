c3eca359909433ab5fc48578e1b226243b15c704
yooncastle <dbstjd427@gmail.com>
2021-04-04 14:18:46 +0900

    Header 글씨 크기 조정

--- portfolio/src/components/Header/HeaderPresenter.js
+++ portfolio/src/components/Header/HeaderPresenter.js
@@ -8,7 +8,8 @@ const Header = styled.header`
     padding: 0 100px;
     background-color: ${props => props.current ? "#fff" : "transparent"};
     transition: all .1s linear;
-    z-index:1;
+    z-index:2;
+    
     color: ${props => props.current ? "rgb(20,20,20)" : "#fff"};
     @media (max-width: 994px){
         padding: 0px;
@@ -24,7 +25,7 @@ const HeaderWrap = styled.nav`
     display: flex;
     position:relative;
     justify-content:space-between;
-    height: 80px;
+    height: 70px;
     
 `;
 const Button = styled.button`
@@ -38,22 +39,22 @@ const Button = styled.button`
     text-align:center;
     @media (max-width: 994px){
         display:block;
-        z-index:1;
+        z-index:2;
     }
 `;
 const Logo = styled.h1`
     padding-top: 15px;
-    font-size: 50px;
+    font-size: 40px;
     height: 100%;
     @media (max-width: 994px){
         margin-left: 30px;
-         
+        z-index:2;
     }
 `;
 
 const NavList = styled.ul`
     display:flex;
-    font-size: 30px;
+    font-size: 25px;
     font-weight:600;
     text-align:center;
     
@@ -65,7 +66,7 @@ const NavList = styled.ul`
         left: 0;
         transition: top .3s ease-in;
         line-height:3;
-         
+         z-index:0;
     }
 `;
 
@@ -107,7 +108,7 @@ const MobCover = styled.div`
     height:100%;
      
     display:none;
-     
+    z-index:1;
     @media (max-width: 994px){
         display:block;
     }
